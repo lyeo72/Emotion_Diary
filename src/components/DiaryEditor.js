495abbdd11418517c34187aebdd15993ca98ac72
lyeo72 <lyeo7272@gmail.com>
2022-05-30 23:44:28 +0900

    일기쓰기 날짜선택창

---
+++ src/components/DiaryEditor.js
@@ -0,0 +1,38 @@
+import MyHeader from "./MyHeader";
+import MyButton from "./MyButton";
+import { useNavigate } from "react-router-dom";
+import { useState } from "react";
+
+const getStringDate = (date) => {
+  return date.toISOString().slice(0, 10);
+};
+
+const DiaryEditor = () => {
+  const [date, setDate] = useState(getStringDate(new Date()));
+  const Navigate = useNavigate();
+  return (
+    <div className="DiaryEditor">
+      <MyHeader
+        headText={"새 일기쓰기"}
+        leftChild={
+          <MyButton text={"< 뒤로가기"} onClick={() => Navigate(-1)} />
+        }
+      />
+      <div>
+        <section>
+          <h4>오늘은 언제인가요?</h4>
+          <div className="input_box">
+            <input
+              className="input_date"
+              value={date}
+              onChange={(e) => setDate(e.target.value)}
+              type="date"
+            />
+          </div>
+        </section>
+      </div>
+    </div>
+  );
+};
+
+export default DiaryEditor;
